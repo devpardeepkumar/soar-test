2231efa1aea1ecec507ce6d18edb94fd637af7ab
pardeepkumar <devpardeepkumar60@gmail.com>
2024-12-20 20:39:40 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 const config                = require('./config/index.config.js');
 const Cortex                = require('ion-cortex');
 const ManagersLoader        = require('./loaders/ManagersLoader.js');
+const Aeon                  = require('aeon-machine');
 
-const mongoDB = config.dotEnv.MONGO_URI? require('./connect/mongo')({
-    uri: config.dotEnv.MONGO_URI
-}):null;
+process.on('uncaughtException', err => {
+    console.log(`Uncaught Exception:`)
+    console.log(err, err.stack);
 
-const cache = require('./cache/cache.dbh')({
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled rejection at ', promise, `reason:`, reason);
+    process.exit(1)
+})
+
+const cache      = require('./cache/cache.dbh.js')({
     prefix: config.dotEnv.CACHE_PREFIX ,
     url: config.dotEnv.CACHE_REDIS
 });
 
-const cortex = new Cortex({
+const Oyster  = require('oyster-db');
+const oyster     = new Oyster({ 
+    url: config.dotEnv.OYSTER_REDIS, 
+	prefix: config.dotEnv.OYSTER_PREFIX 
+});
+
+const cortex     = new Cortex({
     prefix: config.dotEnv.CORTEX_PREFIX,
     url: config.dotEnv.CORTEX_REDIS,
     type: config.dotEnv.CORTEX_TYPE,
     state: ()=>{
         return {} 
     },
-    activeDelay: "50ms",
-    idlDelay: "200ms",
+    activeDelay: "50",
+    idlDelay: "200",
 });
+const aeon = new Aeon({ cortex , timestampFrom: Date.now(), segmantDuration: 500 });
 
-
-
-const managersLoader = new ManagersLoader({config, cache, cortex});
+const managersLoader = new ManagersLoader({config, cache, cortex, oyster, aeon});
 const managers = managersLoader.load();
 
 managers.userServer.run();
